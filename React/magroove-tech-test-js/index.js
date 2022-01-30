303c05a4dab2c6b6df8e3af5af97e74a4f616b07
Luiz Souza <luiz.souz404@gmail.com>
2022-01-30 02:40:57 -0300

    Feat: styling React application

--- React/magroove-tech-test-js/index.js
+++ React/magroove-tech-test-js/index.js
@@ -6,8 +6,12 @@ class ToDoItem extends React.Component {
 
 	render() {
 		return (
-			<li style={this.state.is_done ? { textDecoration: "line-through" } : {}} onClick={() => this.setState({ is_done: !this.state.is_done })}>
+			<li style={this.state.is_done ? styles.liElementChecked : styles.liElement} onClick={() => this.setState({ is_done: !this.state.is_done })}>
 				{this.props.task}
+
+				<div style={this.state.is_done ? styles.divChecked : styles.divCheck}>
+					<img style={this.state.is_done ? styles.checkedImg : styles.checkImg} src="./assets/check.svg" />
+				</div>
 			</li>
 		);
 	}
@@ -27,6 +31,10 @@ class ToDoList extends React.Component {
 	handleClick() {
 		const newArray = this.state.to_do_list;
 
+		if (this.state.input_value === "") {
+			return
+		}
+
 		newArray.push(this.state.input_value);
 
 		this.setState(newArray);
@@ -40,17 +48,129 @@ class ToDoList extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<h2>Add a new task to your to-do list</h2>
-				<input onChange={this.handleChange} value={this.state.input_value} type="text" />
-				<button onClick={() => this.handleClick()}>Add</button>
-				<ul>
-					{this.state.to_do_list.map((task_text, index) =>
-						<ToDoItem key={index} task={task_text} />
-					)}
-				</ul>
-			</div>
+			<main style={styles.container}>
+
+				<div style={styles.content}>
+					<header style={styles.headerContainer}>
+						<img src="./assets/Logo.svg" />
+
+						<label style={styles.inputContainer}>
+							<input style={styles.inputElement} onChange={this.handleChange} value={this.state.input_value} type="text" />
+							<button style={styles.btnElement} onClick={() => this.handleClick()}>Add</button>
+						</label>
+
+					</header>
+					<h2 style={styles.titleText}>Add a new task to your to-do list</h2>
+					<ul style={{ listStyle: 'none' }}>
+						{this.state.to_do_list.map((task_text, index) =>
+							<ToDoItem key={index} task={task_text} />
+						)}
+					</ul>
+				</div>
+
+			</main>
 		);
+	}
+}
+
+const styles = {
+	container: {
+		background: '#1d1b1b',
+		height: '100vh',
+		padding: '1.5rem 0',
+		display: 'flex',
+		alignItems: 'center',
+		flexDirection: 'column'
+	},
+	content: {
+		maxWidth: '1024px',
+		padding: '0 1rem',
+		width: '100%'
+	},
+	headerContainer: {
+		display: 'flex',
+		alignItems: 'center'
+	},
+	titleText: {
+		color: '#fff',
+		margin: '1rem 0',
+	},
+	inputContainer: {
+		display: 'flex',
+		alignItems: 'center',
+		marginLeft: '1rem',
+		width: '100%',
+		height: '2.5rem',
+		background: '#312C2A3D',
+		borderRadius: '2rem',
+		overflow: 'hidden'
+	},
+	inputElement: {
+		width: '90%',
+		flex: 1,
+		height: '100%',
+		padding: '.5rem 1rem',
+		background: 'transparent',
+		border: 0,
+		outline: 0,
+		color: '#fff'
+	},
+	btnElement: {
+		width: '10%',
+		minWidth: '90px',
+		height: '100%',
+		background: '#3D3D3D',
+		color: '#fff',
+		border: 0,
+		outline: 0,
+		cursor: 'pointer'
+	},
+	liElement: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		padding: '.75rem 1rem',
+		background: '#fff',
+		borderRadius: '1rem',
+		margin: '.5rem 0 0',
+		cursor: 'pointer'
+	},
+	liElementChecked: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		padding: '.75rem 1rem',
+		background: '#fff',
+		borderRadius: '1rem',
+		margin: '.5rem 0 0',
+		textDecoration: "line-through",
+		opacity: .5
+	},
+	divCheck: {
+		border: '2px solid #1d1b1b',
+		width: '20px',
+		height: '20px',
+		borderRadius: '5px'
+	},
+	divChecked: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		border: '2px solid #EC6B43',
+		background: '#EC6B43',
+		width: '20px',
+		height: '20px',
+		borderRadius: '5px'
+	},
+	checkImg: {
+		visibility: 'hidden',
+		width: '16px',
+		height: '16px'
+	},
+	checkedImg: {
+		visibility: 'visible',
+		width: '16px',
+		height: '16px',
 	}
 }
 
